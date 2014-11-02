554cefc28079e4a0ebf6f435f9799f9474c47e54
Erik <erik@erik-Lenovo-IdeaPad-Y510P.(none)>
2014-11-02 13:04:13 -0600

    New push

--- compute_highest_affinity.py
+++ compute_highest_affinity.py
@@ -19,26 +19,26 @@ def highest_affinity(site_list, user_list, time_list):
 	max_site1 = ""
 	max_site2 = ""
 	max_count = 0
-	count = 0
 	for site1 in site_dict:
-		count = count + 1
-		if count mode 2:
-			for site2 in site_dict:
-				if (site1 != site2):
-					user_set1 = site_dict[site1]
-					user_set2 = site_dict[site2]
-					test_count = len(user_set1.intersection(user_set2))
-					if test_count > max_count:
-						max_count = test_count
-						if site1 < site2:
-							max_site1 = site1
-							max_site2 = site2
-						else:
-							max_site2 = site1
-							max_site1 = site2
+		for site2 in site_dict:
+			if (site1 != site2):
+				user_set1 = site_dict[site1]
+				user_set2 = site_dict[site2]
+				test_count = len(user_set1.intersection(user_set2))
+				if test_count > max_count:
+					max_count = test_count
+					if site1 < site2:
+						max_site1 = site1
+						max_site2 = site2
+					else:
+						max_site2 = site1
+						max_site1 = site2
 		
 	# Returned string pair should be ordered by dictionary order
 	# I.e., if the highest affinity pair is "foo" and "bar"
 	# return ("bar", "foo"). 
 	#print (max_site1, max_site2)
 	return (max_site1, max_site2)
+
+def unusedMethod():
+	print "This method is not used"
