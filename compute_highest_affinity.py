3e22bc38ac94af4e396604aa4d3d564e139194dc
Erik <erik@erik-Lenovo-IdeaPad-Y510P.(none)>
2014-11-02 12:58:41 -0600

    Changed coverage

--- compute_highest_affinity.py
+++ compute_highest_affinity.py
@@ -19,20 +19,23 @@ def highest_affinity(site_list, user_list, time_list):
 	max_site1 = ""
 	max_site2 = ""
 	max_count = 0
+	count = 0
 	for site1 in site_dict:
-		for site2 in site_dict:
-			if (site1 != site2):
-				user_set1 = site_dict[site1]
-				user_set2 = site_dict[site2]
-				test_count = len(user_set1.intersection(user_set2))
-				if test_count > max_count:
-					max_count = test_count
-					if site1 < site2:
-						max_site1 = site1
-						max_site2 = site2
-					else:
-						max_site2 = site1
-						#max_site1 = site2
+		count = count + 1
+		if count mode 2:
+			for site2 in site_dict:
+				if (site1 != site2):
+					user_set1 = site_dict[site1]
+					user_set2 = site_dict[site2]
+					test_count = len(user_set1.intersection(user_set2))
+					if test_count > max_count:
+						max_count = test_count
+						if site1 < site2:
+							max_site1 = site1
+							max_site2 = site2
+						else:
+							max_site2 = site1
+							max_site1 = site2
 		
 	# Returned string pair should be ordered by dictionary order
 	# I.e., if the highest affinity pair is "foo" and "bar"
